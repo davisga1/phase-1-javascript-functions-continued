1b9998086bb2161f6169be766c58b3e70def5278
GaryDavis <davisga1999@gmail.com>
2022-09-10 04:23:17 -0500

    commit tree d45f8e22ba2585516eefc6aeed154a9ab9f0f216

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(fun){
+fun = typeof fun !== "undefined" ? fun : 'roller-skate'
+ return `This Saturday, I want to ${fun}!`
+  
+}
+function mondayWork(work){
+    work = typeof work !== "undefined" ? work : 'go to the office'
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(y){
+    y !== "undefined" ? y : '*'  
+    function inner(u){
+        u !== "undefined" ? u : 'special'
+        return `You are ${y}${u}${y}!`
+    }
+    return inner
+}
